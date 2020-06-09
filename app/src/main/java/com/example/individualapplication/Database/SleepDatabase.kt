f213ebd909ec4a69a3c0cec5185112397da5c70d
Mo <mohamed.sahla@hva.nl>
2020-06-09 18:28:17 +0200

    Alle classes aangemaakt.
    e.g. nog niet uitgewerkt, basis lijn opgesteld op papier m.b.v. tutorials en examples.

--- app/src/main/java/com/example/individualapplication/Database/SleepDatabase.kt
+++ app/src/main/java/com/example/individualapplication/Database/SleepDatabase.kt
@@ -1,4 +1,70 @@
 package com.example.individualapplication.Database
 
-class SleepDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+/**
+ * Database die SleepNight informatie stored.
+ * een global method aanmaken om toegang tot de
+ * databse te krijgen.
+ */
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase (){
+    /**
+     * connects de database met de DAO
+     */
+
+    abstract val SleepDatabaseDao : SleepDatabaseDao
+
+    /**
+     * je maakt hier een companion object aan, hierdoor kun je functies toevoegen aan de SleepDatabase Class.
+     *
+     * Bijvoorbeeld: clients kunnen een call naar "SleepDatabase.getInstance(context) sturen
+     * om een nieuwe SleepDatabase te instantieren.
+     */
+
+    companion object{
+        /**
+         * INSTANCE laat een reference achter naar de database die wordt gereturned
+         * door getInstance.
+         *
+         * Dit zodat je niet telkens de database moet initialiseren.
+         *
+         * De value van de volatile variable wordt nooit gecached, en alles wat je werite and read wordt gedaan
+         * van en naar de main memory.
+         * wijzigingen die worden gemaakt door een thread kunnen dus ook gezien worden door andere threads.
+         */
+
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context) : SleepDatabase {
+
+
+            synchronized(this){
+
+                var instance= INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+                    //Assign INSTANCE naar de nieuwe database.
+                    INSTANCE = instance
+                }
+
+                //Return instance
+                return instance
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/example/individualapplication/Database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/individualapplication/Database/SleepDatabaseDao.kt
@@ -1,4 +1,36 @@
 package com.example.individualapplication.Database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+/**
+ * Method om de SleepNight class met room te gebruiken.
+ */
+
+@Dao
 interface SleepDatabaseDao {
+
+    @Insert
+    fun insert (night: SleepNight)
+
+    /**
+     * update de row met de value die al geset is
+     * en verander de oude value naar de nieuwe
+     */
+    @Update
+    fun update(night: SleepNight)
+
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long) : SleepNight?
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTnight() : SleepNight?
+
 }
