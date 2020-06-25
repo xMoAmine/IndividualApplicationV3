806bb24c0499157e51c43cec6d48934bbc454ec3
xMoAmine <62362897+xMoAmine@users.noreply.github.com>
2020-06-25 11:18:17 +0200

    Update SleepTrackerFragment.kt

--- app/src/main/java/com/example/individualapplication/sleepTracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/individualapplication/sleepTracker/SleepTrackerFragment.kt
@@ -14,12 +14,6 @@ import com.example.individualapplication.R
 import com.example.individualapplication.databinding.FragmentSleepTrackerBinding
 import com.google.android.material.snackbar.Snackbar
 
-/**
- * A fragment with buttons to record start and end times for sleep, which are saved in
- * a database. Cumulative data is displayed in a simple scrollable TextView.
- * (Because we have not learned about RecyclerView yet.)
- * The Clear button will clear all data from the database.
- */
 class SleepTrackerFragment : Fragment() {
 
     /**
@@ -75,13 +69,7 @@ class SleepTrackerFragment : Fragment() {
         // Add an Observer on the state variable for Navigating when STOP button is pressed.
         sleepTrackerViewModel.navigateToSleepQuality.observe(this, Observer { night ->
             night?.let {
-                // We need to get the navController from this, because button is not ready, and it
-                // just has to be a view. For some reason, this only matters if we hit stop again
-                // after using the back button, not if we hit stop and choose a quality.
-                // Also, in the Navigation Editor, for Quality -> Tracker, check "Inclusive" for
-                // popping the stack to get the correct behavior if we press stop multiple times
-                // followed by back.
-                // Also: https://stackoverflow.com/questions/28929637/difference-and-uses-of-oncreate-oncreateview-and-onactivitycreated-in-fra
+
                 this.findNavController().navigate(
                     SleepTrackerFragmentDirections
                         .actionSleepTrackerFragmentToSleepQualityFragment(night.nightId))
