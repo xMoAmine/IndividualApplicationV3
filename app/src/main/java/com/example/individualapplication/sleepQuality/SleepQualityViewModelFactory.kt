70b2027bb636eac1fec6db1f9d85f6c59be2fd56
xMoAmine <62362897+xMoAmine@users.noreply.github.com>
2021-07-06 19:26:41 +0200

    Update SleepQualityViewModelFactory.kt

--- app/src/main/java/com/example/individualapplication/sleepQuality/SleepQualityViewModelFactory.kt
+++ app/src/main/java/com/example/individualapplication/sleepQuality/SleepQualityViewModelFactory.kt
@@ -4,11 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.individualapplication.Database.SleepDatabaseDao
 
-/**
- * This is pretty much boiler plate code for a ViewModel Factory.
- *
- * Provides the key for the night and the SleepDatabaseDao to the ViewModel.
- */
 class SleepQualityViewModelFactory(
     private val sleepNightKey: Long,
     private val dataSource: SleepDatabaseDao
