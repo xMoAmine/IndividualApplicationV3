15cd18953cf1a7f1d08725a8b354ddab16984c71
xMoAmine <62362897+xMoAmine@users.noreply.github.com>
2021-07-06 19:27:29 +0200

    Update SleepQualityViewModel.kt

--- app/src/main/java/com/example/individualapplication/sleepQuality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/individualapplication/sleepQuality/SleepQualityViewModel.kt
@@ -10,53 +10,32 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-/**
- * ViewModel voor de SleepQualityFragment.
- *
- * @param sleepNightKey is de key van de night die in sessie is..
- */
 class SleepQualityViewModel(
     private val sleepNightKey: Long = 0L,
     val database: SleepDatabaseDao) : ViewModel() {
 
-    /** Coroutine setup variables */
-
-    /**
-     * viewModelJob allows us to cancel all coroutines started by this ViewModel.
-     */
     private val viewModelJob = Job()
 
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
 
-    /**
-     * When true immediately navigate back to the [SleepTrackerFragment]
-     */
+    
     val navigateToSleepTracker: LiveData<Boolean?>
         get() = _navigateToSleepTracker
 
-    /**
-     * Cleart de View Model
-     */
+    
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
     }
 
 
-    /**
-     * Call this immediately after navigating to [SleepTrackerFragment]
-     */
+
     fun doneNavigating() {
         _navigateToSleepTracker.value = null
     }
 
-    /**
-     * Sets the sleep quality and updates the database.
-     *
-     * Then navigates back to the SleepTrackerFragment.
-     */
     fun onSetSleepQuality(quality: Int) {
         uiScope.launch {
             // IO is aeen thread pool om operaties op te runnen
